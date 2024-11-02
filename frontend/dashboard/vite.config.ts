4efa87b969377a80fcf4fa3a350111492aed0285
Kohei Fujino <appleblueberrycherry@gmail.com>
2024-11-02 20:04:01 +0900

    add base path

--- frontend/dashboard/vite.config.ts
+++ frontend/dashboard/vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -9,9 +11,19 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
+  base: process.env.VITE_REPOSITORY_NAME!,
   plugins: [
     remix({
       ssr: false,
+      basename: process.env.VITE_REPOSITORY_NAME!,
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
